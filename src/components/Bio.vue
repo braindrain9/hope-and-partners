34929f8673a9d72b8043891ba4963800ff157b2b
Kateryna Vitruk <katherina.vitruk@gmail.com>
2018-12-29 23:40:59 +0200

    updated bio - add real text, add Event component. update header padding

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -24,31 +24,128 @@
 					</div>
 					<div class="description-block col-md-7 col-sm-6">
 						<div class="description description-dark">
-							Тут має бути текст про досягнення, написаний копірайтерами, з назвами гучних компаній і проектів.
-							Так дійсно легше для сприйняття.
-							Проте моя робота не про легкі, готові рішення.
-							Як терапевт, допомагаю вам долати виклики у комунікації – бренду, його піарі та репутації.
-							Аби ви були впливовими у своїх сферах, а не лише популярними.
-							Були, а не здавалися.
-							<br/>
-							<br/>
-							<br/>
+							<div>
+								Я спеціаліст зв’язків з реальністю, це жарт наполовину.
+								Бо допомагаю проектам розібратися в комунікаціях – маркетингу,
+								брендингу та піарі самотужки, показати реальний стан справ.
+							</div>
 
-							To his mother’s delight, Tobias has been honored with the net magazine Designer
-							of the Year & Awwwards Art Director of the Year awards.
-							He also served as a member on the Board of Directors at AIGA NYC and is a supporter,
-							advisor and mentor to a variety of programs around the world, including Cannes Lions, HyperIsland, Art Directors Club & the FWA.
-							<br/>
-							<br/>
-							<br/>
+							<div>
+								<h4>Громадсько-політична сфера</h4>
+								<div>
+									З початку кар’єри займалася соціальними й громадськими проектами, деколи паралельно з бізнесовими.
+									Найвагоміші з них:
+									<br />
+									<br />
+									З 2015 й до сьогодні консультант у проекті
+									<a href="https://www.facebook.com/InvisibleBattalion/" target="_blank">«Невидимий Батальйон»</a>.
+									Це правозахисний рух жінок-військових. У результаті його діяльності
+									в тому числі прийнято рамковий Закон України «Щодо забезпечення рівних прав і можливостей жінок
+									і чоловіків під час проходження військової служби у Збройних Силах України», 09.09.2018.
+									<br />
+									<br />
+									Національний викладач у <a href="https://www.facebook.com/ualeadershipacademy/" target="_blank">Українська Академія Лідерства</a>
+									(навчальний заклад формату «школа лідерів»,
+									де вчаться випускники підлітки, відібрані на конкурсній основі) та
+									куратор проекту Академії стажування молоді у органах влади
+									<a href="https://www.facebook.com/derzhavotvorets/" target="_blank">«Державотворець»</a>
+									(спільно з USAID).
+									<br />
+									<br />
+									2016-2017 керівник комунікації київського представництва партії «Самопоміч»,
+									включаючи депутатський корпус у Київраді (22 депутати).
+								</div>
+							</div>
 
-							Tobias is the co-founder of Semplice™﻿, a portfolio system and community of the world’s
-							leading designers who represent brands such as HUGE, BBDO, Tumblr, Disney, Apple, Firstborn,
-							Unit9, R/GA, Razorfish and many more. Since its launch in 2014, Semplice has quickly grown
-							into one of the leading portfolio systems for creatives.
+							<div>
+								<h4>Освіта</h4>
+
+								<div>
+									Я усвідомлений викладач, який кайфує від своєї роботи. Намагаюся вчити думати, а не копіювати чужі історії успіху.
+									<br />
+									<br />
+									З 2017 до сьогодні – автор і викладач курсу комунікацій у бізнес-школі
+									<a href="https://prjctr.com.ua/marketing-communications.html" target="_blank">Projector</a>.
+									<br />
+									<br />
+
+									2017 – ментор інкубатору для соціальних стартапів
+									<a href="http://www.1991.vc/about/" target="_blank">1991 Open Data Incubator</a>
+									(реалізується за підтримки міжнародних організацій WNISEF, USAID і Уряду України) і
+									<a href="http://selab.chasopys.ua/" target="_blank">SELab школи соціального підприємництва</a>
+									(реалізується за підтримки Social Entrepreneurship Eco System, Швеція).
+									<br />
+									<br />
+
+									З 2017 й до сьогодні – друг і радник наукової платформи Nobilitet (все про Нобелівську премію й не тільки)
+									<br />
+									<br />
+
+									2013 – 2016 член команди організаторів міжнародної конференції <a href="https://tedxkyiv.com/" target="_blank">TEDxKyiv</a>
+									<br />
+									<br />
+
+									Ініціювала проведення в Україні саме жіночої гілки конференції
+									<a href="https://www.youtube.com/watch?v=7pGseTL2sp0" target="_blank">TEDxWomen</a> й виступила там як
+									<a href="https://www.youtube.com/watch?v=7pGseTL2sp0" target="_blank">спікер</a> (2017).
+								</div>
+							</div>
+
+							<div>
+								<h4>Бізнес</h4>
+								<div>
+									Пункти вище стали можливі в тому числі завдяки кар’єрі в креативних індустріях та власній справі:
+									<br />
+									<br />
+									2018 – заснувала консалтингове бюро Hope&Partners
+									<br />
+									<br />
+									2016 – 2018 засновниця і співвласниця закладу <a href="https://www.facebook.com/prbarkyiv" target="_blank">PR Bar</a>
+									<br />
+									<br />
+									2015 – маркетинг-директор Сім’ї ресторанів <a href="https://borysov.com.ua/" target="_blank">Діми Борисова</a>
+									<br />
+									<br />
+									2011–2013, координатор проектів рекламно-маркетингової агенції <a href="https://borysov.com.ua/" target="_blank">FEDORIV</a>.
+									<br />
+									<br />
+									2009-2011, фрілансер, культурні й соціальні проекти.
+									<br />
+									<br />
+									2007–2009, PR менеджер міжнародного рекламного агенства <a href="http://havaswwukraine.com.ua/" target="_blank">Havas Worldwide Ukraine</a>.
+									<br />
+									<br />
+								</div>
+							</div>
+
+							<div>
+								<h4>Відзнаки</h4>
+								<div>
+									Шорт-лист <a href="http://diplomat.media/2018/10/02/european-women-expert-awards-2018/" target="_blank">European Women Expert Awards 2018</a>
+									<br />
+									<br />
+
+									<a href="https://womanpress.jimdo.com/top-woman-500/business-services-1/" target="_blank">ТОП 500 «найвпливовіших жінок України»</a>
+									за версією видання WOMAN у категорії «бізнес».
+									<br />
+									<br />
+
+									Випускниця <a href="http://aspeninstitutekyiv.org/" target="_blank">Аспен Інститут Київ</a>, семінар 11.2018
+									<br />
+									<br />
+
+									Кращий ментор табору для підприємців
+									<a href="http://startupukraine.kiev.ua/campowners" target="_blank">Startup Ukraine 2017</a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+			</b-container>
+		</div>
+		<div class="event-container">
+			<b-container>
+				<Event />
 			</b-container>
 		</div>
 	</div>
@@ -56,13 +153,18 @@
 
 <script>
   import bus from '../bus';
+  import Event from './Event';
 
   export default {
     name: 'Bio',
 
     created: function () {
       bus.$emit('toggleLoading', false);
-    }
+    },
+
+		components: {
+      Event
+		}
   }
 </script>
 
@@ -117,10 +219,29 @@
 				margin-top: 50px;
 
 				.description {
-					font-weight: 500;
-					line-height: 28px;
-					font-size: 20px;
+					max-width: 600px;
+
+					div {
+						font-weight: 500;
+						line-height: 27px;
+						font-size: 20px;
+						margin-bottom: 60px;
+					}
+
+					h4 {
+						font-weight: bold;
+						font-size: 24px;
+						margin-bottom: 15px;
+					}
 				}
+			}
+		}
+
+		.event-container {
+			padding: 160px 0 40px;
+
+			.container {
+				max-width: 920px;
 			}
 		}
 	}
