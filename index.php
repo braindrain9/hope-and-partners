faffe35468f6e7b86412742a4297dc9cd66120f1
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-02-17 01:25:32 +0200

    update index meta tags

--- index.php
+++ index.php
@@ -8,18 +8,15 @@
    <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700">
    <meta name="viewport"
         content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-   <meta name="description"
-        content="Just a simple Vue blog that displays posts via the WordPress REST API. Fork it and rip it up!">
-   <meta name="keywords" content="Vue,JavaScript,WordPress,REST API">
-   <meta property="og:site_name" content="WP Vue"/>
-   <meta property="og:locale" content="en_us"/>
-   <meta property="og:description"
-        content="Just a simple Vue blog that displays posts via the WordPress REST API. Fork it and rip it up!"/>
-   <meta property="og:title" content="WP Vue"/>
+   <meta name="description" content="Маркетингова агенція яка налагоджує зв'язки з реальністю">
+   <meta property="og:site_name" content="Hope & Partners"/>
+   <meta property="og:locale" content="ua_ua"/>
+   <meta property="og:description" content="Маркетингова агенція яка налагоджує зв'язки з реальністю"/>
+   <meta property="og:title" content="Hope & Partners"/>
    <meta property="og:type" content="website"/>
    <meta name="twitter:card" content="summary"/>
-   <meta name="twitter:title" content="About"/>
-   <meta name="twitter:description" content="Just a simple Vue blog that displays posts via the WordPress REST API."/>
+   <meta name="twitter:title" content="Hope & Partners"/>
+   <meta name="twitter:description" content="Маркетингова агенція яка налагоджує зв'язки з реальністю"/>
   <?php wp_head(); ?>
 </head>
 <body>
