650323afc7a5aba71fed6c9395e638676438f12d
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-02-18 13:27:12 +0200

    fix font loading

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
    <title>Hope & Partners</title>
    <link rel='shortcut icon' type='image/x-icon' href='favicon.ico'/>
    <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700">
    <meta name="viewport"
         content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
    <meta name="description" content="Маркетингова агенція яка налагоджує зв'язки з реальністю">
