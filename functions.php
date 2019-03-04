3f73306e2949b9e473dfd255da9f188681a67a20
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-03-04 15:10:46 +0200

    update functions php and htaceess

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function remove_admin_bar_menus() {
     $wp_admin_bar->remove_menu('updates');
     $wp_admin_bar->remove_menu('comments');
     $wp_admin_bar->remove_menu('customize');
+    $wp_admin_bar->remove_menu('view');
+    $wp_admin_bar->remove_menu('imagify');
 }
 add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_menus' );
 
@@ -27,18 +29,38 @@ add_action('init', 'remove_redirects');
 
 // Load scripts
 function load_vue_scripts() {
-    wp_enqueue_script('blankslate/vendor.js', get_template_directory_uri() . '/dist/vendor.js', null, null, true);
-    wp_enqueue_script('blankslate/bundle.js', get_template_directory_uri() . '/dist/bundle.js', null, null, true);
-    wp_enqueue_script('blankslate/font.json', get_template_directory_uri() . '/dist/src/assets/json/Montserrat_Bold.json', null, null, true);
+    wp_enqueue_script('vendor.js', get_template_directory_uri() . '/dist/vendor.js', null, null, true);
+    wp_enqueue_script('bundle.js', get_template_directory_uri() . '/dist/bundle.js', null, null, true);
+    wp_enqueue_script('font.json', get_template_directory_uri() . '/dist/src/assets/json/Montserrat_Bold.json', null, null, true);
 }
 add_action('wp_enqueue_scripts', 'load_vue_scripts', 100);
 
-add_filter( 'clean_url', function( $url )
-{
-    if ( FALSE === strpos( $url, '.js' ) )
-    { // not our file
-        return $url;
+function add_defer_attribute($tag, $handle) {
+    // add script handles to the array below
+   $scripts_to_defer = array('vendor.js', 'bundle.js', 'font.json');
+
+   foreach($scripts_to_defer as $defer_script) {
+      if ($defer_script === $handle) {
+         return str_replace(' src', ' defer="defer" src', $tag);
+      }
+   }
+   return $tag;
+}
+add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
+
+function remove_embeded_scripts(){
+ wp_deregister_script('wp-embed');
+}
+add_action( 'wp_footer', 'remove_embeded_scripts' );
+
+function remove_jquery() {
+    if( !is_admin()){
+     wp_deregister_script('jquery');
     }
-    // Must be a ', not "!
-    return "$url' defer='defer";
-}, 11, 1 );
+}
+
+add_action( 'wp_enqueue_scripts', 'remove_jquery' );
+
+add_action('wp_enqueue_scripts', function() {
+  wp_dequeue_style( 'wp-block-library' );
+});
