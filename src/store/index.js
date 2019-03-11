46dbfbab071410f55de163ae8fcbb64af43a29b9
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-03-11 16:47:43 +0200

    refactoring - remove unused Card.vue, store and modules, axios.

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import bus from '../bus';
-import modules from './modules'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    endpoint: REST_ENDPOINT,
-    content_endpoint: CONTENT_ENDPOINT,
-    sliding: true,
+    content_endpoint: 'https://hopeandpart.com/wp-content/themes/wp-vue/',
     content: {}
   },
 
   mutations: {
-    updateEndpoint (state, endpoint) {
-      state.endpoint = endpoint;
-      bus.$emit('clearError');
-      bus.$emit('bumpViewKey', 'Updating content...');
-    },
-    updateSliding (state, value) {
-      state.sliding = value;
-    },
     updateGeneralContent (state, content) {
       state.content = content;
     }
-  },
-
-  modules
+  }
 });
